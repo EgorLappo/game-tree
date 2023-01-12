beb4aa55311cea97aad3d9e3c8be4159e68ae7cf
Egor Alimpiev <alimpiev@stanford.edu>
2023-01-12 11:07:50 -0800

    it's working!

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
+
+import System.Directory
+import Control.Monad
+import Data.List.Split
 
 import Lib
 
 main :: IO ()
-main = undefined
+main = let datadir = "data"
+           countdir = "counts"
+           n = 3
+       in do 
+            files <- listDir datadir -- list dir
+            createDirectoryIfMissing True countdir
+            countdirs <- forM files $ \f -> do
+                let cd = countdir ++ "/"++ fileName f ++ "/"
+                createDirectoryIfMissing True cd
+                return cd
+            return ()
+            
+            forM_ (zip files countdirs) $ \(f,d) -> do
+              tree <- buildTreeFromFile n (datadir ++ "/" ++ f)
+              writeCountMap d $ traverseTreeCounts tree "cnts"
+            
+            
+-- get the name with no extensions   
+fileName :: String -> String
+fileName = head . splitOn "."
+
+listDir :: FilePath -> IO [FilePath]
+listDir dir = do
+    files <- listDirectory dir
+    return $ filter (\s -> head s /= '.') files
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,34 +1,33 @@
-module Lib where
+{-# LANGUAGE OverloadedStrings #-}
+
+module Lib (Tree, CountMap, buildTreeFromFile, traverseTreeCounts, writeCountMap) where
 
 import qualified Data.ByteString.Lazy.Char8 as BS 
--- import qualified Data.Text as T
--- import qualified Data.Text.IO as TIO
+import Data.List (foldl', foldl1')
+import qualified Data.HashMap.Strict as H
+import Control.Monad (forM_)
 
 data Tree = Tree { move   :: BS.ByteString
                  , count  :: Int
                  , leaves :: [Tree] }
+  deriving (Show, Eq)
 
-processFile :: Int -- tree depth
+-- *** BUILDING THE GAME TREE ***
+
+buildTreeFromFile :: Int -- tree depth
           -> FilePath -- file path
           -> IO Tree -- game tree with counts
-processFile n = (foldl (traverseTree) (Tree (BS.pack "start") 0 []) . map (getMoves n) . BS.lines <$>) . BS.readFile 
-
-processRow :: Int -- tree depth
-           -> Tree -- curent game tree
-           -> BS.ByteString -- pgn string
-           -> Tree 
-processRow n t pgn = traverseTree t moves
-    where moves = getMoves n pgn
+buildTreeFromFile n = (foldl' traverseTreeBuild (Tree "start" 0 []) . map (getMoves n) . BS.lines <$>) . BS.readFile 
 
 --base recursive function
-traverseTree :: Tree -> [BS.ByteString] -> Tree
-traverseTree (Tree m n ls) [] = Tree m (n+1) ls
-traverseTree (Tree m n ls) (r:rs) = Tree m (n+1) ls' 
+traverseTreeBuild :: Tree -> [BS.ByteString] -> Tree
+traverseTreeBuild (Tree m n ls) [] = Tree m (n+1) ls
+traverseTreeBuild (Tree m n ls) (r:rs) = Tree m (n+1) ls' 
     where ls' | r `elem` map move ls = go r ls -- move already there -> go over leaves and modify the right one
-              | otherwise = traverseTree (Tree r 0 []) rs : ls -- move not there -> append a new leaf to the tree
+              | otherwise = traverseTreeBuild (Tree r 0 []) rs : ls -- move not there -> append a new leaf to the tree
 
           go _ [] = []
-          go r' (y:ys) | r' == move y = traverseTree y rs : ys -- right leaf -> kep inserting the rest to that leaf
+          go r' (y:ys) | r' == move y = traverseTreeBuild y rs : ys -- right leaf -> kep inserting the rest to that leaf
                        | otherwise = y : go r' ys -- wrong leaf -> go to the next leaf
 
 -- process pgn string (neatly formatted) 
@@ -37,7 +36,44 @@ getMoves :: Int -> BS.ByteString -> [BS.ByteString]
 getMoves n = take n . dropEvery 3 . BS.words
 
 -- drop every nth element starting by dropping first
-dropEvery :: Int -> [a] -> [a]
-dropEvery _ [] = []
-dropEvery n xs = take (n-1) (drop 1 xs) ++ drop n (dropEvery n xs)
+dropEvery:: Int -> [a] -> [a]
+dropEvery k = go k k
+  where go _ _ []     = []
+        go i n (x:xs) = if i == n
+         then go 1 n xs
+         else x : go (i+1) n xs
+
+
+
+-- *** PRINTING THE RESULTS
+
+newtype Counts = Counts { counts :: [(BS.ByteString, BS.ByteString)] }
+  deriving (Show)
+type CountMap = H.HashMap BS.ByteString Counts
+
+
+extractCounts :: Tree -> Counts 
+extractCounts t = Counts $ map (\m -> (move m, BS.pack $ show $ count m)) $ leaves t
+
+traverseTreeCounts :: Tree -> BS.ByteString -> CountMap
+traverseTreeCounts t pos 
+  | null (leaves t) = H.empty -- we are at a leaf
+  | otherwise = let curCounts = extractCounts t 
+                    recurCounts = flip map (leaves t) $ \t' ->
+                      traverseTreeCounts t' (BS.unwords [pos, move t'])
+                in H.insert pos curCounts $ foldl1' H.union recurCounts
+
+writeCountMap :: FilePath -> CountMap ->  IO ()
+writeCountMap dir m = do
+  forM_ (H.toList m) $ \(pos, cnt) -> 
+    let file = dir ++ "/" ++ BS.unpack pos
+        (names, nums) = unzip $ counts cnt
+        cntStr = BS.unlines [BS.intercalate (BS.pack ", ") names, BS.intercalate (BS.pack ", ") nums]
+    in BS.writeFile file cntStr
+
+-- writeCounts :: String -> String -> Tree -> IO ()
+-- writeCounts file tag tree = BS.writeFile file $ BS.unlines [names', nums']
+--     where (names, nums) = unzip . map (\m -> (move m, BS.pack $ show $ count m)) $ leaves tree
+--           names' = BS.intercalate (BS.pack ", ") $ "tag" : names 
+--           nums'  = BS.intercalate (BS.pack ", ") $ BS.pack tag : nums 
 
